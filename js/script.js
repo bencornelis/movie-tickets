75e42080ed6884bc95dcab2e4f3a638694ba882c
Drew <drew@finstrom.us>
2015-06-02 16:04:58 -0700

    add randomized movie times at page load and when changing movie titles

--- js/script.js
+++ js/script.js
@@ -1,14 +1,91 @@
 //raw js
+function Movie(name, isFirstRelease) {
+  this.name = name;
+  this.firstRelease = isFirstRelease;
+  this.showtimes = [];
+};
+
+
+Movie.prototype.addShowtime = function(showtime) {
+  this.showtimes.push(showtime);
+};
+
+
+function Showtime(hrs, mins) {
+  this.hours = hrs;
+  this.minutes = mins;
+};
+
+Showtime.prototype.isPM = function() {
+  return this.hours >= 12;
+};
+
+Showtime.prototype.civFormat = function() {
+  var ampm = "AM";
+  if (this.isPM()) { ampm = "PM"};
+  return this.hours % 12 + ":" + this.minutes + " " + ampm;
+};
+
+Showtime.prototype.milFormat = function() {
+  return this.hours + ":" + this.minutes;
+};
+
+function Ticket(movieName, isFirstRelease, showtime, patronAge) {
+  this.matineeDiscount = 1;
+  this.seniorDiscount = 2;
+  this.movieName = movieName;
+  this.firstRelease = isFirstRelease;
+  this.matinee = showtime.hours < 4;
+  this.senior = patronAge >= 65;
+};
+
+Ticket.prototype.cost = function() {
+  var ticketCost;
+  if (this.firstRelease) {
+    ticketCost = 10;
+  } else {
+    ticketCost = 6;
+  }
+
+  if (this.matinee) {
+    ticketCost -= this.matineeDiscount;
+  }
+
+  if (this.senior) {
+    ticketCost -= this.seniorDiscount;
+  }
+  return ticketCost;
+};
+
 var foo = function(bar) {
   "use strict";
   return false;
 };
 
+var randomTime = function() {
+  "user strict";
+  var hour = Math.floor((Math.random() * 24) + 1);
+  var min = Math.floor((Math.random() * 5) + 1) * 10;
+  return new Showtime(hour, min);
+}
+
+var populateMovieTimes = function() {
+  $("select#showtimes").empty();
+  for (var i = 0; i < 3; i++) {
+    var someTime = randomTime();
+    var optionHTML = "<option value='" + someTime.milFormat() + "'>" + someTime.civFormat() + "</option>";
+    $("select#showtimes").append(optionHTML);
+  }
+}
 
 //jQuery
 $( document ).ready(function() {
   "use strict";
     console.log( "jQuery Ready" );
 
-    $("#jqtest").text("jQuery Ready");
+    populateMovieTimes();
+
+    $("select#movie-title").change(function() {
+      populateMovieTimes();
+    });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,85 @@
-describe("foo", function() {
+// describe("Movie", function() {
+//   "use strict";
+//   it("has a name, release type, and initially empty array of showtimes", function() {
+//     var testMovie = new Movie("Inception", false)
+//     expect(testMovie.name).to.equal("Inception");
+//     expect(testMovie.firstRelease).to.equal(false);
+//     expect(testMovie.showtimes).to.eql([]);
+//   });
+//
+//   describe("addShowtime", function() {
+//     it("adds another showtime to a movie", function() {
+//       var testMovie = new Movie("Inception", false)
+//       testMovie.addShowtime("4:00 PM");
+//       expect(testMovie.showtimes).to.eql(["4:00 PM"]);
+//     });
+//   });
+//
+// });
+
+describe("Showtime", function() {
   "use strict";
-  it("is some function or expectation", function() {
-    expect(foo("bar")).to.equal(false);
+  it("has hours, minutes", function() {
+    var testShowTime = new Showtime(4, 20)
+    expect(testShowTime.hours).to.equal(4);
+    expect(testShowTime.minutes).to.equal(20);
+  });
+
+  describe("isPM", function() {
+    "use strict";
+    it("time is in the PM", function() {
+      var testShowTime = new Showtime(20, 20)
+      expect(testShowTime.isPM()).to.equal(true);
+    });
+  });
+
+  describe("milFormat", function() {
+    "use strict";
+    it("time formatted in 24 hour notation", function() {
+      var testShowTime = new Showtime(20, 20)
+      expect(testShowTime.milFormat()).to.equal("20:20");
+    });
+  });
+
+  describe("civFormat", function() {
+    "use strict";
+    it("time formatted in 24 hour notation", function() {
+      var testShowTime = new Showtime(20, 20)
+      expect(testShowTime.civFormat()).to.equal("8:20 PM");
+    });
+  });
+
+});
+
+describe("Ticket", function() {
+  "use strict";
+  it("has a movie name, release type, show time, senior discount", function() {
+    var testTime = new Showtime(7, 45);
+    var testTicket = new Ticket("Godzilla", false, testTime, 21);
+    expect(testTicket.movieName).to.equal("Godzilla");
+    expect(testTicket.firstRelease).to.equal(false);
+    expect(testTicket.matinee).to.equal(false);
+    expect(testTicket.senior).to.equal(false);
+  });
+
+  describe("#cost", function() {
+    "use strict";
+    it("calculates the ticket cost factoring in release type of a movie", function() {
+      var testTime = new Showtime(7, 45);
+      var testTicket = new Ticket("Godzilla", true, testTime, 21);
+      expect(testTicket.cost()).to.equal(10);
+    });
+
+    it("factors in the showtime", function() {
+      var testTime = new Showtime(3, 45);
+      var testTicket = new Ticket("Godzilla", false, testTime, 21);
+      expect(testTicket.cost()).to.equal(5);
+    });
+
+    it("factors in the age of the moviegoer", function() {
+      var testTime = new Showtime(3, 45);
+      var testTicket = new Ticket("Godzilla", false, testTime, 66);
+      expect(testTicket.cost()).to.equal(3);
+    });
   });
 });
